d38009b2f9b3cba021f54ccfcb8c0c17d61255e2
Crazy_Fox <holmyliszag@ukr.net>
2026-06-08 10:38:19 +0300

    fix main

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from cmath import phase
-from math import fabs
-from os import name
-
 
 comands = ['"hello" - to start',
           '"exit" or "close" - to close programm',
@@ -18,31 +14,35 @@ def parse_comand(user_input: str):
 
 def add_contact(args: list, contacts: dict):
     if len(args) != 2:
-        return False
+        return "Invalid command. Use the 'help' command to get valid commands."
     name, phone = args
-    if name in contacts:
-        return "contact exist"
     contacts[name] = phone
     return "Caontact added."
 
 def change_contact(args: list, contacts: dict):
     if len(args) != 2:
-        return False
+        return "Invalid command. Use the 'help' command to get valid commands."
     name, phone = args
     if name in contacts:
         contacts[name] = phone
         return "Contact changed"
-    return "not found"
+    return "Contact not found"
     
 
 def get_phone(args: list, contacts: dict):
     if len(args) != 1:
-        return False
+        return "Invalid command. Use the 'help' command to get valid commands."
     name = args[0]
     if name in contacts:
         return f"Name: {name}; Phone: {contacts[name]}"
     else:
         return "Contact not found"
+
+def get_all(contacts: list):
+    if len(contacts) > 0:
+        return contacts
+    else:
+        return "Your contacts is empty..."
 
 def main():
     contacts = {}
@@ -60,55 +60,13 @@ def main():
             for comand in comands:
                 print(comand)
         elif comand == "all":
-            if len(contacts) > 0:
-                for name in contacts:
-                    print(f"Name: {name}, Phone: {contacts[name]}")
-            else:
-                print("Your contacts is empty...")
+            print(get_all(contacts))
         elif comand == "add":
-            res =  add_contact(list_arg, contacts)
-            if res:
-                if res == "contact exist":
-                    print("This contact already exists. Would you like to change it?")
-                    while True:
-                        cmd = input("Enter Yes or No: ").strip().lower()
-                        if cmd == "yes":
-                            print(change_contact(list_arg, contacts))
-                            break
-                        elif cmd == "no":
-                            print("You have cancelled the contact change.")
-                            break
-                        else:
-                            print("Invalid comand.")
-                else:
-                    print(res)
-            else:
-                print("Invalid command. Use the 'help' command to get valid commands.")
+            print(add_contact(list_arg, contacts))
         elif comand == "phone":
-            res = get_phone(list_arg, contacts)
-            if res:
-                print(res)
-            else:
-                print("Invalid command. Use the 'help' command to get valid commands.")
+            print(get_phone(list_arg, contacts))
         elif comand == "change":
-            res = change_contact(list_arg, contacts)
-            if res:
-                if res == "not found":
-                    print("This contact doesn't exist. Perhaps you wanted to add a new one?")
-                    while True:
-                        cmd = input("Enter Yes or No: ").strip().lower()
-                        if cmd == "yes":
-                            print(add_contact(list_arg, contacts))
-                            break
-                        elif cmd == "no":
-                            print("You have canceled adding a contact.")
-                            break
-                        else:
-                            print("Invalid comand")
-                else:
-                    print(res)
-            else:
-                print("Invalid command. Use the 'help' command to get valid commands.")
+            print(change_contact(list_arg, contacts))
         else:
             print("Invalid command. Use the 'help' command to get valid commands.")
 
